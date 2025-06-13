b647acc8e842e94d9e1898bdf447b1c7ac38c270
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-06-13 17:33:50 +0530

    Time: 44 ms (78.36%) | Memory: 113.4 MB (64.37%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,82 @@
+class LRUCache {
+
+    private class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final int capacity;
+    private final Map<Integer, Node> cache;
+    private final Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+        this.head = new Node(0, 0); // Dummy head
+        this.tail = new Node(0, 0); // Dummy tail
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (!cache.containsKey(key)) {
+            return -1;
+        }
+        Node node = cache.get(key);
+        moveToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.value = value;
+            moveToHead(node);
+        } else {
+            Node newNode = new Node(key, value);
+            cache.put(key, newNode);
+            addNode(newNode);
+
+            if (cache.size() > capacity) {
+                Node lru = removeTail();
+                cache.remove(lru.key);
+            }
+        }
+    }
+
+    private void addNode(Node node) {
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void moveToHead(Node node) {
+        removeNode(node);
+        addNode(node);
+    }
+
+    private Node removeTail() {
+        Node lru = tail.prev;
+        removeNode(lru);
+        return lru;
+    }
+}
+
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
