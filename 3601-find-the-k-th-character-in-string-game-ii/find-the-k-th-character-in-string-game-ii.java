51883105a38b32df7ec6a862d68661c9d100a7c7
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-07-04 16:07:03 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (45.12%) - LeetSync

---
+++ 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.java
@@ -0,0 +1,7 @@
+class Solution {
+    public char kthCharacter(long k, int[] operations) {
+        int c = 0; k--;
+        for (int i = 0; k != 0; i++, k >>= 1) c += ((int)(k & 1) & operations[i]);
+        return (char)((c % 26) + 'a');
+    }
+}
