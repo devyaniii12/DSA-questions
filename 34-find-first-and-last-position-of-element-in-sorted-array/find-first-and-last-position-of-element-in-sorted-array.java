855189034fd474255a8d5e53d7557a76a2a85d98
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-07-15 23:16:48 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (16.74%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,32 +1,30 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int ans[]={-1,-1};
-        int start=search(nums,target,true);
-        int end=search(nums,target,false);
-        ans[0]=start;
-        ans[1]=end;
-        return ans;
-    
+        int[] arr=new int[2];
+        arr[0]=search(nums,target,true);
+        arr[1]=search(nums,target,false);
+        return arr;
     }
-    public int search(int[] nums,int target,boolean findStartIndex){
-        int ans=-1;
-        int start=0;
-        int end=nums.length-1;
+    int search(int[] nums,int target,boolean flag){
+        int start=0;int end=nums.length-1;int ans=-1;
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(target>nums[mid]){
+            if(nums[mid]<target){
                 start=mid+1;
-            }else if(target<nums[mid]){
+            }
+            else if(nums[mid]>target){
                 end=mid-1;
-            }else{
-                ans=mid;
-                if(findStartIndex){
+            }  
+            else{
+                ans=mid;//potential ans
+                if(flag){
                     end=mid-1;
                 }
                 else{
                     start=mid+1;
                 }
+            }      
             }
+            return ans;
         }
-        return ans;
-    }}
+    }
