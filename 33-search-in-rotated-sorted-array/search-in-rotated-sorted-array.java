44e4fdd01fe94a24bb4cfee0e8135a8eb8a4ce7d
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-07-16 23:29:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (68.11%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,26 +1,36 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int pivot=findPivot(nums);
-        if(pivot==-1){
-            //array is already sorted
-            return binarySearch(nums,target,0,nums.length-1);
-            
+        int n=nums.length;
+        int start=0;int end=n-1;
+        int minId=min(nums);
+        int ans1=-1;
+        if(minId>0){
+             ans1=bs(nums,target,0,minId-1);
         }
-        if(nums[pivot]==target){
-            return pivot;
+        int ans2=bs(nums,target,minId,end);
+        if(ans2<0){
+            return ans1;
         }
-        if(target<nums[0]){
-            return binarySearch(nums,target,pivot+1,nums.length-1);
-
-        }
-       
-            return binarySearch(nums,target,0,pivot-1);
-      
-
+        else{
+            return ans2;
+        } 
+     
     }
-
-    public int binarySearch(int[] nums,int target,int start,int end){
-        
+    public int min(int[] nums){
+        int n=nums.length;
+        int start=0;int end=n-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]>nums[end]){
+                start=mid+1;
+            }
+            else{
+                end=mid;
+            }
+        }
+        return start;
+    }
+    int bs(int[] nums,int target,int start,int end){
         while(start<=end){
             int mid=start+(end-start)/2;
             if(nums[mid]>target){
@@ -34,30 +44,5 @@ class Solution {
             }
         }
         return -1;
-    }
-
-    public int findPivot(int[] nums){
-        int start=0;
-        int end=nums.length-1;
-        while(start<=end){
-            
-            int mid=start+(end-start)/2;
-            if(mid<end && nums[mid]>nums[mid+1]){
-                return mid;
-
-            } 
-            if(mid>start && nums[mid]<nums[mid-1]){
-                return mid-1;
-            }
-            if(nums[mid]<=nums[start]){
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
-            }
-
-        }
-        return -1;
-        
     }
 }
