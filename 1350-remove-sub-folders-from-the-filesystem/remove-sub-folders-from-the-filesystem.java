5d92f8b741c1dea67a8d27434356efe5ce872736
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-07-19 23:19:33 +0530

    Time: 42 ms (70.51%) | Memory: 55.6 MB (57.03%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
@@ -3,9 +3,9 @@ class Solution {
         Arrays.sort(folder);
         List<String> ans=new ArrayList<>();
         ans.add(folder[0]);
-        for(int i = 1;i<folder.length;i++) {
+        for(int i = 1;i < folder.length;i++) {
             if(!folder[i].startsWith(ans.get(ans.size()-1) + "/")){
-                ans.add(folder[i]);
+                ans.add(folder [i]);
             }
         }
         return ans;
