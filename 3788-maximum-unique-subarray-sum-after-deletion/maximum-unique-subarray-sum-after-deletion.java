eaf3060cec05d9f82c43c29cfaed5d206963f1a2
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-07-25 20:46:54 +0530

    Time: 2 ms (74.06%) | Memory: 42.5 MB (64.69%) - LeetSync

--- 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.java
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.java
@@ -1,27 +1,32 @@
 class Solution {
     public int maxSum(int[] nums) {
-        boolean allNegative=true;
-        int maxValue=Integer.MIN_VALUE;
-        for(int n:nums){
-            if(n >= 0){
-                allNegative=false;
+        boolean allNegative = true;
+        int maxValue = Integer.MIN_VALUE;
+        for (int n : nums) {
+            if (n >= 0) {
+                allNegative = false;
             }
-            if(n > maxValue){
-                maxValue=n;
+            if (n > maxValue) {
+                maxValue = n;
             }
         }
         if (allNegative)
             return maxValue;
-        Set<Integer> unique=new HashSet<>();
-        for(int n:nums){
+
+        // Use a set to remove duplicates
+        Set<Integer> unique = new HashSet<>();
+        for (int n : nums) {
             unique.add(n);
         }
+
+        // Sum all positive numbers from the set
         int sum = 0;
-        for(int val:unique){
-            if(val>0){
-                sum+=val;
+        for (int val : unique) {
+            if (val > 0) {
+                sum += val;
             }
         }
+
         return sum;
     }
 }
