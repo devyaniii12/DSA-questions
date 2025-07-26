f6765e51949c01f6034b87dfdf6deb7dc9495e0c
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-07-26 17:12:45 +0530

    Time: 59 ms (74.36%) | Memory: 144.4 MB (15.38%) - LeetSync

--- 3789-maximize-subarrays-after-removing-one-conflicting-pair/maximize-subarrays-after-removing-one-conflicting-pair.java
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/maximize-subarrays-after-removing-one-conflicting-pair.java
@@ -1,12 +1,13 @@
 class Solution {
     public long maxSubarrays(int n, int[][] conflictingPairs) {
-        List<Integer>[] right = new ArrayList[n + 1];
+       List<Integer>[] right = new ArrayList[n + 1];
         for (int i = 0; i <= n; i++) {
             right[i] = new ArrayList<>();
         }
         for (int[] pair : conflictingPairs) {
             right[Math.max(pair[0], pair[1])].add(Math.min(pair[0], pair[1]));
         }
+
         long ans = 0;
         long[] left = {0, 0}; // left[0] is top1, left[1] is top2
         long[] bonus = new long[n + 1];
@@ -21,16 +22,19 @@ class Solution {
                     left[1] = l_val;
                 }
             }
+
             ans += r - left[0];
             
             if (left[0] > 0) {
                 bonus[(int)left[0]] += left[0] - left[1];
             }
         }
+        
         long maxBonus = 0;
         for (long b : bonus) {
             maxBonus = Math.max(maxBonus, b);
         }
-        return ans + maxBonus;
+
+        return ans + maxBonus; 
     }
 }
