16540907dac89d092a9d68c231a8d6e329796a5f
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-08-15 22:33:00 +0530

    Time: 1 ms (77.31%) | Memory: 45.7 MB (85.63%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,18 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        
-        
-        int maxProfit = 0;
-        if (prices == null || prices.length < 2) {
-            maxProfit=0; // No profit can be made if there's 0 or 1 day
-        }
-
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                maxProfit += prices[i] - prices[i - 1];
+        int maxP=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]>prices[i-1]){
+                maxP+=prices[i]-prices[i-1];
             }
         }
-        
-        return maxProfit;
+        return maxP;
     }
 }
