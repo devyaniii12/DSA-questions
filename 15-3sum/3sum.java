37f3f21549aa43f0c86829da0bf9bcbf4a071f23
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-08-16 16:55:06 +0530

    Time: 31 ms (56.72%) | Memory: 51.6 MB (57.03%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -19,19 +19,50 @@ class Solution {
 
 
         //Better Approach -  HashSet
-        Set<List<Integer>> s=new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-        Set<Integer> set=new HashSet<>();
-        for(int j=i+1;j<nums.length;j++){
-             int third=-nums[i]-nums[j];
-            if(set.contains(third)){
-                int temp[]={nums[i],nums[j],third};
-                Arrays.sort(temp);
-                s.add(Arrays.asList(temp[0],temp[1],temp[2]));
+        // Set<List<Integer>> s=new HashSet<>();
+        // for(int i=0;i<nums.length;i++){
+        // Set<Integer> set=new HashSet<>();
+        // for(int j=i+1;j<nums.length;j++){
+        //      int third=-nums[i]-nums[j];
+        //     if(set.contains(third)){
+        //         int temp[]={nums[i],nums[j],third};
+        //         Arrays.sort(temp);
+        //         s.add(Arrays.asList(temp[0],temp[1],temp[2]));
+        //     }
+        //     set.add(nums[j]);
+        // }
+        // }
+        // return new ArrayList<>(s);
+
+
+
+
+
+        //Best - 2 pointers
+        Arrays.sort(nums);
+        List<List<Integer>> ans =new ArrayList<>();
+        for(int i=0;i<nums.length-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int left=i+1;
+            int right=nums.length-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum==0){
+                    ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    while(left<right && nums[left]==nums[left+1]) left++;
+                    while(left<right && nums[right]==nums[right-1]) right--;
+                left++;
+                right--;
+                }
+                else if(sum<0){
+                    left++;
+                }
+                else{
+                    right--;
+                }
             }
-            set.add(nums[j]);
         }
-}
- return new ArrayList<>(s);
+        return ans;
+
     }
 }
