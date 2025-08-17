5df17daed6973eaec9910ac5dc8deba11a43e352
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-08-17 13:12:00 +0530

    Time: 0 ms (100.00%) | Memory: 57.7 MB (14.25%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,6 +1,37 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n=nums.length;
+        //brute- extra array temp
+    // int n = nums.length;
+    // int[] temp = new int[n];
+    // for(int i = 0; i < n; i++) {
+    //     temp[(i + k) % n] = nums[i];
+    // }
+    // for(int i = 0; i < n; i++) {
+    //     nums[i] = temp[i];
+    // }
+
+
+
+    //better - cyclic replacement
+    // int n = nums.length;
+    // k = k % n;
+    // int count = 0; // number of elements moved
+    // for (int start = 0; count < n; start++) {
+    //     int current = start;
+    //     int prev = nums[start];
+    //     do {
+    //         int next = (current + k) % n;
+    //         int temp = nums[next];
+    //         nums[next] = prev;
+    //         prev = temp;
+    //         current = next;
+    //         count++;
+    //     } while (start != current);
+    // }
+
+
+    //Optimal-reversal algorithm
+     int n=nums.length;
         k=k%n;
         rev(nums,0,n-1);
         rev(nums,0,k-1);
