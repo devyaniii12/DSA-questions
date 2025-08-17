198b5a42fcdf3a8001823e8bdf51d4f9523b6531
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-08-17 06:09:07 +0530

    Time: 2 ms (87.19%) | Memory: 55.4 MB (61.68%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -17,22 +17,40 @@ class Solution {
 
 
         //better- using prefix and suffix arrays
+        // int n=nums.length;
+        // int[] left=new int[n];
+        // int[] right=new int[n];
+        // int[] ans=new int[n];
+        // left[0]=1;
+        // for(int i=1;i<n;i++){
+        //     left[i]=left[i-1]*nums[i-1];
+        // }
+
+        // right[n-1]=1;
+        // for(int i=n-2;i>=0;i--){
+        //     right[i]=right[i+1]*nums[i+1];
+        // }
+
+        // for(int i=0;i<n;i++){
+        //     ans[i]=left[i]*right[i];
+        // }
+        // return ans;
+
+
+
+
+
+        //optimal- using ans array to store left array multiples 
         int n=nums.length;
-        int[] left=new int[n];
-        int[] right=new int[n];
+        int suffix=1;
         int[] ans=new int[n];
-        left[0]=1;
+        ans[0]=1;
         for(int i=1;i<n;i++){
-            left[i]=left[i-1]*nums[i-1];
+            ans[i]=ans[i-1]*nums[i-1];
         }
-
-        right[n-1]=1;
-        for(int i=n-2;i>=0;i--){
-            right[i]=right[i+1]*nums[i+1];
-        }
-
-        for(int i=0;i<n;i++){
-            ans[i]=left[i]*right[i];
+        for(int i=n-1;i>=0;i--){
+            ans[i]=ans[i]*suffix;
+            suffix=suffix*nums[i];
         }
         return ans;
     }
