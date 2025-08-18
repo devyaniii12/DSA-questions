8b0919d45cf67998bc40b5c0caee650e92ddc14e
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-08-18 18:14:51 +0530

    Time: 6 ms (73.30%) | Memory: 45.2 MB (14.95%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -24,15 +24,30 @@ class Solution {
 
 
     //Better-using set with bruteforce
-    int longMax=0;
-    for(int i=0;i<s.length();i++){
-        Set<Character> set=new HashSet<>();
-        int j=i;
-        while(j<s.length() && !set.contains(s.charAt(j))){
-            set.add(s.charAt(j));
-            longMax=Math.max(longMax,j-i+1);
-            j++;
+    // int longMax=0;
+    // for(int i=0;i<s.length();i++){
+    //     Set<Character> set=new HashSet<>();
+    //     int j=i;
+    //     while(j<s.length() && !set.contains(s.charAt(j))){
+    //         set.add(s.charAt(j));
+    //         longMax=Math.max(longMax,j-i+1);
+    //         j++;
+    //     }
+    // }
+    // return longMax;
+
+
+    //Optimal - with two pointers
+    int n=s.length();
+    Set<Character> set=new HashSet<>();
+    int left=0, longMax=0;
+    for(int right=0;right<n;right++){
+        while(set.contains(s.charAt(right))){
+            set.remove(s.charAt(left));
+            left++;
         }
+        set.add(s.charAt(right));
+        longMax=Math.max(longMax,right-left+1);
     }
     return longMax;
     }
