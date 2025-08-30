e407da91fa76c628a5cf171f8033a39439c2c317
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-08-30 11:35:22 +0530

    Time: 3 ms (52.39%) | Memory: 44.4 MB (66.47%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,30 +1,46 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        //for rows
         for(int i=0;i<9;i++){
+            Set<Character> set=new HashSet<>();
             for(int j=0;j<9;j++){
-                if(!isValid(i,j,board)){
-                    return false;
-                }
+                if(board[i][j]=='.') continue;
+                if(set.contains(board[i][j])) return false;
+                set.add(board[i][j]);
+            }
+        }
+
+        //for cols
+        for(int j=0;j<9;j++){
+            Set<Character> set=new HashSet<>();
+            for(int i=0;i<9;i++){
+                if(board[i][j]=='.') continue;
+                if(set.contains(board[i][j])) return false;
+                set.add(board[i][j]);
+            }
+        }
+
+        // for box
+        for(int sr=0;sr<9;sr+=3){
+            int er=sr+2;
+            for(int sc=0;sc<9;sc+=3){
+                int ec=sc+2;
+                if(!(isValid(board,sr,er,sc,ec))) return false;
             }
         }
         return true;
     }
-    private boolean isValid(int row,int col, char[][]board){
-        if(board[row][col]=='.'){
-            return true;
-        }
-        for(int i=0;i<9;i++){
-             if(row!=i && board[i][col]==board[row][col]){
-            return false;
-        }
-        if(col!=i && board[row][i]==board[row][col]){
-            return false;
-        }
-        if((3*(row/3)+i/3)!=row && (3*(col/3)+i%3)!=col && board[(3*(row/3)+i/3)][(3*(col/3)+i%3)]==board[row][col]){
-            return false;
-        }
+
+    boolean isValid(char[][]board,int sr,int er,int sc,int ec){
+        Set<Character> set=new HashSet<>();
+        for(int i=sr;i<=er;i++){
+            for(int j=sc;j<=ec;j++){
+                if(board[i][j]=='.') continue;
+
+                if(set.contains(board[i][j])) return false;
+                set.add(board[i][j]);
+            }
         }
         return true;
-       
     }
 }
