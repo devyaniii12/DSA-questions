385e202dc8c7d2279521fec777a895f34aab66c3
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-08-31 13:22:21 +0530

    Time: 369 ms (37.15%) | Memory: 41.6 MB (12.38%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -2,41 +2,28 @@ class Solution {
     public void solveSudoku(char[][] board) {
         solve(board);
     }
-
     boolean solve(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 if (board[i][j] == '.') {
-                    // Try placing digits '1' to '9'
-                    for (char k = '1'; k <= '9'; k++) {
-                        if (isValid(board, i, j, k)) {
-                            board[i][j] = k;
-
-                            // Recursively solve the rest of the board
-                            if (solve(board)) {
-                                return true;
-                            } else {
-                                // Undo the move if it leads to a dead end
-                                board[i][j] = '.';
-                            }
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+                            if (solve(board)) return true;
+                            board[i][j] = '.'; // backtrack
                         }
                     }
-                    // If no digit can be placed in this cell, return false
                     return false;
                 }
             }
         }
-        // If the entire board is filled, return true
         return true;
     }
-
-    boolean isValid(char[][] board, int row, int col, char k) {
-        // Check if 'k' is valid in the row, column, and 3x3 grid
-        for (int c = 0; c < 9; c++) {
-            if (board[row][c] == k || board[c][col] == k || 
-                board[3 * (row / 3) + c / 3][3 * (col / 3) + c % 3] == k) {
-                return false;
-            }
+    boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c) return false;
+            if (board[i][col] == c) return false;
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
         }
         return true;
     }
