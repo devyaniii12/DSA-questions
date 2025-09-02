552f487260249277b50b65bba9548cd2ca5db644
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-09-02 20:37:15 +0530

    Time: 1 ms (100.00%) | Memory: 42.2 MB (96.75%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,20 +1,70 @@
 class Solution {
     public int myAtoi(String s) {
-        int i = 0, n = s.length(), sign = 1;
-        long num = 0; 
-        while (i < n && s.charAt(i) == ' ') {
-            i++;
-        }
+        //Brute- using inbuilt functions
+        // s = s.trim();
+        // if (s.isEmpty()) return 0;
+
+        // try {
+        //     return Integer.parseInt(s);
+        // } catch (NumberFormatException e) {
+        //     if (s.charAt(0) == '-') return Integer.MIN_VALUE;
+        //     else return Integer.MAX_VALUE;
+        // }
+
+
+        //Better
+        // int i = 0, n = s.length();
+        // // 1. Skip leading whitespaces
+        // while (i < n && s.charAt(i) == ' ') i++;
+
+        // // 2. Handle sign
+        // int sign = 1;
+        // if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+        //     sign = (s.charAt(i) == '-') ? -1 : 1;
+        //     i++;
+        // }
+
+        // // 3. Convert digits
+        // long result = 0;
+        // while (i < n && Character.isDigit(s.charAt(i))) {
+        //     result = result * 10 + (s.charAt(i) - '0');
+
+        //     // 4. Overflow check
+        //     if (sign == 1 && result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        //     if (sign == -1 && -result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+        //     i++;
+        // }
+
+        // return (int)(sign * result);
+
+
+
+        //Optimal-
+        int i = 0, n = s.length();
+        while (i < n && s.charAt(i) == ' ') i++;
+
+        int sign = 1;
         if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
             sign = (s.charAt(i) == '-') ? -1 : 1;
             i++;
         }
+
+        int result = 0;
         while (i < n && Character.isDigit(s.charAt(i))) {
-            num = num * 10 + (s.charAt(i) - '0');
-            if (num * sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            if (num * sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            int digit = s.charAt(i) - '0';
+
+            // Overflow check before adding digit
+            if (result > (Integer.MAX_VALUE - digit) / 10) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            result = result * 10 + digit;
             i++;
         }
-        return (int)(num*sign);
+
+        return result * sign;
+
+  
     }
 }
