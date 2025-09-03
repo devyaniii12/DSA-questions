63c8649b0e3416a8bbba63f007a13201ee9132e2
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-09-03 10:55:51 +0530

    Time: 46 ms (100.00%) | Memory: 45.5 MB (43.48%) - LeetSync

---
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+class Solution {
+    public int numberOfPairs(int[][] points) {
+        Arrays.parallelSort(points, (p, q) -> {
+            return p[0]==q[0]?(p[1]-q[1]):(q[0]-p[0]);
+        });
+        final int n=points.length;
+        int ans=0;
+        for(int i=0; i<n-1; i++){
+            int y=Integer.MAX_VALUE, yi=points[i][1];
+            for(int j=i+1; j<n; j++){
+                final int yj=points[j][1];
+                if(yj>=yi && y>yj){
+                    ans++;
+                    y=yj;
+                    if(yi==yj) break;
+                }
+            }
+        }
+        return ans;
+    }
+}
