b40ddcae8e61bb102aa40e28be4ba6ffb73a3852
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-09-03 11:54:16 +0530

    Time: 13 ms (60.53%) | Memory: 44.9 MB (27.63%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.java
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.java
@@ -1,32 +1,29 @@
 class Solution {
     public int numberOfPairs(int[][] points) {
-       int n = points.length;
-        int count = 0;
+        int count=0;
+        int n=points.length;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
+        for(int i=0; i<n; i++) {
+            int x1=points[i][0],y1=points[i][1];
+            for(int j=0; j<n; j++) {
+                if(i==j) continue;
+                int x2=points[j][0],y2=points[j][1];
+                if(x1>x2 || y1<y2) continue;
 
-                int[] A = points[i];
-                int[] B = points[j];
+                boolean flag=true;
+                for(int k=0; k<n; k++) {
+                    if(k==i || k==j) continue;
+                    int x=points[k][0],y=points[k][1];
 
-                // A should be upper-left of B
-                if (A[0] <= B[0] && A[1] >= B[1]) {
-                    boolean valid = true;
-                    // check all other points
-                    for (int k = 0; k < n; k++) {
-                        if (k == i || k == j) continue;
-                        int[] P = points[k];
-                        if (P[0] >= A[0] && P[0] <= B[0] &&
-                            P[1] <= A[1] && P[1] >= B[1]) {
-                            valid = false;
-                            break;
-                        }
-                    }
-                    if (valid) count++;
+                    if(x>=x1 && x<=x2 && y<=y1 && y>=y2) {
+                        flag=false;
+                        break;
+                    }                        
                 }
+                if(flag==true) count++;
             }
         }
-        return count; 
+
+        return count;
     }
 }
