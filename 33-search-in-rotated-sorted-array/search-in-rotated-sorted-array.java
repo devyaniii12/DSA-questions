30977a930aba6e2df152a00b6671d1b2986377d5
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-09-14 15:52:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (6.46%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,47 +1,36 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n=nums.length;
-        int start=0;int end=n-1;
-        int minId=min(nums);
-        int ans1=-1;
-        if(minId>0){
-             ans1=bs(nums,target,0,minId-1);
+        int peak=pivot(nums);
+        int ans=binarySearch(nums,target,0,peak);
+        if (ans==-1){
+            ans=binarySearch(nums,target,peak+1,nums.length-1);
         }
-        int ans2=bs(nums,target,minId,end);
-        if(ans2<0){
-            return ans1;
-        }
-        else{
-            return ans2;
-        } 
-     
+        return ans;
     }
-    public int min(int[] nums){
-        int n=nums.length;
-        int start=0;int end=n-1;
-        while(start<end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]>nums[end]){
-                start=mid+1;
-            }
-            else{
-                end=mid;
-            }
-        }
-        return start;
-    }
-    int bs(int[] nums,int target,int start,int end){
+    int pivot(int[] nums){
+        int start=0;int end=nums.length-1;
+        if(nums[start]<nums[end]) return -1;
         while(start<=end){
             int mid=start+(end-start)/2;
+            if(mid<end && nums[mid]>nums[mid+1]) return mid;
+            else if(mid>start && nums[mid]<nums[mid-1]) return mid-1;
+
+            else if(nums[mid]>=nums[start]) start=mid+1;
+            else end=mid-1;
+            
+        }
+        return -1;
+    }
+    int binarySearch(int[] nums,int target,int start,int end){
+        while(start<=end){
+             int mid=start+(end-start)/2;
             if(nums[mid]>target){
                 end=mid-1;
             }
             else if(nums[mid]<target){
                 start=mid+1;
             }
-            else{
-                return mid;
-            }
+            else return mid;
         }
         return -1;
     }
