b1b56ce14d683bba38fd5b797948414c148e20cc
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-09-26 12:54:36 +0530

    Time: 1 ms (80.87%) | Memory: 41.8 MB (57.82%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -3,14 +3,9 @@ class Solution {
         StringBuilder ans=new StringBuilder();
         int n=word1.length();
         int m=word2.length();
-        int i=0;int j=0;
-        while(i<n || j<m){
-            if(i<n){
-                ans.append(word1.charAt(i++));
-            }
-            if(j<m){
-                ans.append(word2.charAt(j++));
-            }
+        for(int i=0;i<Math.max(n,m);i++){
+            if(i<n) ans.append(word1.charAt(i));
+            if(i<m) ans.append(word2.charAt(i));
         }
     return ans.toString();
 }
