336fb162169137542a1114183ac3067bd74368d2
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-10-29 13:48:14 +0530

    Time: 272 ms (6.60%) | Memory: 44.8 MB (74.31%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,31 +1,13 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        //Brute- T.C=O(N*N) S.C=O(1)
-        // int maxP=Integer.MIN_VALUE;
-        // for(int i=0;i<nums.length;i++){
-        //     int p=1;
-        //     for(int j=i;j<nums.length;j++){
-        //         p*=nums[j];
-        //         maxP=Math.max(maxP,p);
-        //     }
-        // }
-        // return maxP;
-
-            //Optimal -Prefix–Suffix Algorithm
-            // T.C=O(N) S.C=O(1)
-            int n=nums.length;
-            int maxP=Integer.MIN_VALUE;
-            int prefix=1;int suffix=1;
-            for(int i=0;i<nums.length;i++){
-                if(prefix==0){
-                    prefix=1;
-                }
-                if(suffix==0) suffix=1;
-                prefix=prefix*nums[i];
-                suffix=suffix*nums[n-i-1];
-                maxP=Math.max(maxP,Math.max(prefix,suffix));
+        int maxP=Integer.MIN_VALUE;;
+        for(int i=0;i<nums.length;i++){
+            int p=1;
+            for(int j=i;j<nums.length;j++){
+                p=p*nums[j];
+                maxP=Math.max(maxP,p);
             }
-            return maxP;
-
+        }
+        return maxP;
     }
 }
