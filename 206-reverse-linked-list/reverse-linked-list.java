3e6b04df214b33b342274e17ac8c3d8b3a49a3ad
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-12-02 23:42:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (19.49%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -10,15 +10,8 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        //Recursive approach - T.C=O(N) S.C=O(N)
-        // if(head==null || head.next==null) return head;
-        // ListNode newNode=reverseList(head.next);
-        // head.next.next=head;
-        // head.next=null;
-        // return newNode;
-
-        //prev curr agla approach
-        ListNode prev=null,agla=null;
+        ListNode prev=null;
+        ListNode agla=null;
         ListNode curr=head;
         while(curr!=null){
             agla=curr.next;
