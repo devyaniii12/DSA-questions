f80876a2e066f684e1f8d7955cad4dabf30c8788
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-12-21 23:19:54 +0530

    Time: 8 ms (19.71%) | Memory: 45.7 MB (50.78%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,54 +1,23 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        //Brute- generate all substrings
-    //     int n= s.length();
-    //     int longMax=0;
-    //     for(int i=0;i<n;i++){
-    //         for(int j=i;j<n;j++){
-    //             if(unique(s,i,j)) {
-    //                 longMax=Math.max(longMax,j-i+1);
-    //             }
-    //         }
-    //     }
-    // return longMax;
-    // }
-
-    // boolean unique(String s,int i,int j){
-    //     boolean[] c=new boolean[128];
-    //     for(int k=i;k<=j;k++){
-    //         char ch=s.charAt(k);
-    //         if(c[ch]==true) return false;
-    //         c[ch]=true;
-    //     }
-    //     return true;
-
-
-    //Better-using set with bruteforce
-    // int longMax=0;
-    // for(int i=0;i<s.length();i++){
-    //     Set<Character> set=new HashSet<>();
-    //     int j=i;
-    //     while(j<s.length() && !set.contains(s.charAt(j))){
-    //         set.add(s.charAt(j));
-    //         longMax=Math.max(longMax,j-i+1);
-    //         j++;
-    //     }
-    // }
-    // return longMax;
-
-
-    //Optimal - with two pointers
-    int n=s.length();
-    Set<Character> set=new HashSet<>();
-    int left=0, longMax=0;
-    for(int right=0;right<n;right++){
-        while(set.contains(s.charAt(right))){
-            set.remove(s.charAt(left));
-            left++;
+        int low=0;
+        int res=0;
+        Map<Character,Integer> map=new HashMap<>();
+        for(int high=0;high<s.length();high++){
+            char ch=s.charAt(high);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+            int k=high-low+1;
+            while(map.size()<k){
+                char leftCh=s.charAt(low);
+                map.put(leftCh,map.get(leftCh)-1);
+                if(map.get(leftCh)==0){
+                    map.remove(leftCh);
+                }
+                low++;
+                k=high-low+1;
+            }
+            res=Math.max(res,high-low+1);
         }
-        set.add(s.charAt(right));
-        longMax=Math.max(longMax,right-left+1);
-    }
-    return longMax;
+        return res;
     }
 }
