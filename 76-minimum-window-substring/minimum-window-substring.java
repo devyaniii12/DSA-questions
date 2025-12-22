f66db9821232b628f59032c9930d1947ae1cd7b8
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-12-23 01:00:58 +0530

    Time: 65 ms (17.21%) | Memory: 46.6 MB (55.39%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,42 +1,37 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int m = s.length();
-        int n = t.length();
-
-        if (m < n) return "";
-
-        int[] freqS = new int[256];
-        int[] freqT = new int[256];
-
-        for (int i = 0; i < n; i++) {
+        int m=s.length();
+        int n=t.length();
+        if(m<n) return "";
+        int[] freqT=new int[256];
+        for(int i=0;i<n;i++){
             freqT[t.charAt(i)]++;
         }
 
-        int low = 0;
-        int start = -1;
-        int res = Integer.MAX_VALUE;
+        int start=-1;
+        int low=0;
+        int res=Integer.MAX_VALUE;
+        int[] freqS=new int[256];
 
-        for (int high = 0; high < m; high++) {
+        for(int high=0;high<m;high++){
             freqS[s.charAt(high)]++;
 
-            while (sahi(freqS, freqT)) {
-                if (high - low + 1 < res) {
-                    res = high - low + 1;
-                    start = low;
+            while(sahi(freqS,freqT)){
+                if(high-low+1<res){
+                    res=high-low+1;
+                    start=low;
                 }
-
-                // shrink window
                 freqS[s.charAt(low)]--;
                 low++;
             }
         }
-
-        return start == -1 ? "" : s.substring(start, start + res);
+        return start==-1?"":s.substring(start,start+res);
     }
-
-    boolean sahi(int[] freqS, int[] freqT) {
-        for (int i = 0; i < 256; i++) {
-            if (freqS[i] < freqT[i]) return false;
+    boolean sahi(int[] freqS,int[] freqT){
+        for(int i=0;i<256;i++){
+            if(freqS[i]<freqT[i]){
+                return false;
+            }
         }
         return true;
     }
