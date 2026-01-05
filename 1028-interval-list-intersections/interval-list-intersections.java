c1e0487150e2885a60454d819f2261881016145d
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2026-01-05 23:36:21 +0530

    Time: 3 ms (98.82%) | Memory: 47.4 MB (57.19%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.java
+++ 1028-interval-list-intersections/interval-list-intersections.java
@@ -1,16 +1,26 @@
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-        List<int[]> res = new ArrayList<>();
+        List<int[]> ans = new ArrayList<>();
         int i = 0, j = 0;
 
         while (i < firstList.length && j < secondList.length) {
+
             int start = Math.max(firstList[i][0], secondList[j][0]);
-            int end = Math.min(firstList[i][1], secondList[j][1]);
-            if (start <= end) res.add(new int[]{start, end});
-            if (firstList[i][1] < secondList[j][1]) i++;
-            else j++;
+            int end   = Math.min(firstList[i][1], secondList[j][1]);
+
+            // Check if intersection exists
+            if (start <= end) {
+                ans.add(new int[]{start, end});
+            }
+
+            // Move the pointer which ends first
+            if (firstList[i][1] < secondList[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
         }
 
-        return res.toArray(new int[res.size()][]);
+        return ans.toArray(new int[ans.size()][]); 
     }
 }
