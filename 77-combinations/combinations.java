5595eeef3302f96cc0efbe578ecd22cc69b32656
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2026-02-04 22:40:21 +0530

    Time: 17 ms (87.77%) | Memory: 98 MB (80.75%) - LeetSync

---
+++ 77-combinations/combinations.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        helper(1,n,k,list,ans);
+        return ans;
+    }
+    void helper(int start,int n,int k,List<Integer> list,List<List<Integer>> ans){
+        if(k==0){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        if(start>n) return;
+
+helper(start+1,n,k,list,ans);
+        list.add(start);
+        helper(start+1,n,k-1,list,ans);
+        list.remove(list.size()-1);
+    }
+}
