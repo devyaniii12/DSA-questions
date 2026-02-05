d90ba0d5b8ab20ebd1be5ee8cc0b5ec3ea76ec77
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2026-02-05 10:59:46 +0530

    Time: 8 ms (16.56%) | Memory: 45.5 MB (26.41%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,33 +1,26 @@
 class Solution {
-    void  combSum(int ind,int[] arr,List<Integer> list,List<List<Integer>> ans,int target,int sum){
-          
-            if(sum==target){
-                ans.add(new ArrayList<>(list));
-                return;
-            }
-            for(int i=ind; i<arr.length;i++){
-                if(i>ind && arr[i]==arr[i-1]){
-                    continue;
-                }
-                if(sum+arr[i]>target){
-                    break;
-                }
-                
-            list.add(arr[i]);
-           
-            combSum(i+1,arr,list,ans,target,sum+arr[i]);
-            list.remove(list.size()-1);
-         
-        
-            }
-
-            
-        
-    }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(candidates);
-        combSum(0,candidates,new ArrayList<> (),ans,target,0);
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        helper(0,candidates,0,target,list,ans);
         return ans;
+    }
+    void helper(int start,int[] candidates,int sum,int target,List<Integer> list,List<List<Integer>> ans){
+        if(sum==target){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        if(sum>target){
+            return;
+        }
+        for(int i=start;i<candidates.length;i++){
+            if(i>start && candidates[i]==candidates[i-1]){
+                continue;
+            }
+            list.add(candidates[i]);
+            helper(i+1,candidates,sum+candidates[i],target,list,ans);
+            list.remove(list.size()-1);
+        }
     }
 }
