0480113559cdf0fb93988a9fda36d8bcebb72d89
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2026-03-12 12:29:10 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (87.72%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -14,59 +14,39 @@
  * }
  */
 class Solution {
-    TreeNode delete(TreeNode root,int key){
-        if(root==null) return null;
-        if(root.val>key){
-            //go left
-            if(root.left==null) return root;
-            if(root.left.val==key){
-                TreeNode l=root.left;
-                if(l.left==null && l.right==null) root.left=null;
-                else if(l.left==null || l.right==null){
-                    if (l.right != null) root.left = l.right;
-                    else root.left = l.left;
+    public TreeNode deleteNode(TreeNode root, int key) {
+                if(root == null) return null;
 
-                }
-                else{//2 child
-                    TreeNode curr=l;
-                    TreeNode pred=curr.left;
-                    while(pred.right!=null) pred=pred.right;
-                    delete(root,pred.val);
-                    pred.left=curr.left;
-                    pred.right=curr.right;
-                    root.left=pred;
-                }
-            }
-            else delete(root.left,key);
+        if(key < root.val){
+            root.left = deleteNode(root.left, key);
+        }
+        else if(key > root.val){
+            root.right = deleteNode(root.right, key);
         }
         else{
-            if(root.right==null) return root;
-            if(root.right.val==key){
-                TreeNode r=root.right;
-                if(r.left==null && r.right==null) root.right=null;
-                else if(r.left==null || r.right==null){
-                    if(r.left!=null) root.right=r.left;
-                    else root.right=r.right;
-                }
-                else{//2 child
-                    TreeNode curr=r;
-                    TreeNode pred=curr.left;
-                    while(pred.right!=null) pred=pred.right;
-                    delete(root,pred.val);
-                    pred.left=curr.left;
-                    pred.right=curr.right;
-                    root.right=pred;
-                }
+
+            // Case 1: no child
+            if(root.left == null && root.right == null){
+                return null;
             }
-            else delete(root.right,key);
+
+            // Case 2: one child
+            if(root.left == null) return root.right;
+            if(root.right == null) return root.left;
+
+            // Case 3: two children
+            TreeNode successor = findMin(root.right);
+            root.val = successor.val;
+            root.right = deleteNode(root.right, successor.val);
         }
+
         return root;
     }
-    public TreeNode deleteNode(TreeNode root, int key) {
-        TreeNode temp=new TreeNode(Integer.MAX_VALUE);
-        temp.left=root;
-        delete(temp,key);
-        root=temp.left;
-        return root;
-}
+
+    public TreeNode findMin(TreeNode node){
+        while(node.left != null){
+            node = node.left;
+        }
+        return node;
+    }
 }
