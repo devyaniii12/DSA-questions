bcf0223988ad8cd4f7cba1621833c2d9d412edce
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2026-03-26 20:49:02 +0530

    Time: 1 ms (64.33%) | Memory: 47.1 MB (69.58%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int[] visited=new int[graph.length];
+        Arrays.fill(visited,-1);
+        
+        for(int i=0;i<graph.length;i++){
+            if(visited[i]==-1){
+                if(!bfs(i,graph,visited)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    boolean bfs(int node,int[][] graph,int[] visited){
+        Queue<Integer> q=new LinkedList<>();
+        q.add(node);
+        visited[node]=0;
+
+        while(!q.isEmpty()){
+            int ele=q.poll();
+            
+            for(int nei:graph[ele]){
+                if(visited[nei]==-1){
+                    visited[nei]=1-visited[ele];
+                    q.add(nei);
+                }
+                else if(visited[nei]==visited[ele]){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+}
