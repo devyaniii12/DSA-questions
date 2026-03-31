3a6f042c35ad05d4a52359b2752e1398f89edad7
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2026-03-31 12:27:19 +0530

    Time: 7 ms (60.57%) | Memory: 47.2 MB (40.42%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean canFinish(int n, int[][] prerequisites) {
         List<List<Integer>> adj=new ArrayList<>();
-        List<Integer> ans=new ArrayList<>();
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
         int[] indegree=new int[n];
-        for(int[] arr:prerequisites){
-            int u=arr[0];
-            int v=arr[1];
+        for(int i[]: prerequisites){
+            int u=i[0];
+            int v=i[1];
             adj.get(v).add(u);
             indegree[u]++;
         }
@@ -17,25 +16,18 @@ class Solution {
         for(int i=0;i<n;i++){
             if(indegree[i]==0) q.add(i);
         }
-        int idx=0;
+
+ int idx=0;
         while(!q.isEmpty()){
             int ele=q.poll();
-            ans.add(ele);
-            idx++;
+           idx++;
 
             for(int i:adj.get(ele)){
                 indegree[i]--;
                 if(indegree[i]==0) q.add(i);
             }
-
         }
-
-        if(idx!=n){
-            return false;
-        }
+        if(idx!=n) return false;
         return true;
-
-        
-        
     }
 }
