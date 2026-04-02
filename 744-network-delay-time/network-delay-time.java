22897cc66cf8acc5d15324c758550321df7e3c14
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2026-04-02 17:10:08 +0530

    Time: 11 ms (67.91%) | Memory: 49.2 MB (52.43%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.java
@@ -0,0 +1,46 @@
+class Pair{
+    int node,time;
+    Pair(int node,int time){
+        this.node=node;
+        this.time=time;
+    }
+}
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int src) {
+       List<List<Pair>> adj=new ArrayList<>();
+       for(int i=0;i<=n;i++){
+        adj.add(new ArrayList<Pair>());
+       } 
+       for(int i=0;i<times.length;i++){
+        int node=times[i][0];
+        int neigh=times[i][1];
+        int time=times[i][2];
+        adj.get(node).add(new Pair(neigh,time));
+       }
+       
+       int[] ans=new int[n+1];
+       Arrays.fill(ans,Integer.MAX_VALUE);
+       ans[src]=0;
+       PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.time-b.time);
+       pq.add(new Pair(src,0));
+
+       while(pq.size()>0){
+        Pair p=pq.remove();
+        int node=p.node;
+        
+        for(Pair pr:adj.get(node)){
+            int totalTime=p.time+pr.time;
+            if(totalTime<ans[pr.node]){
+                ans[pr.node]=totalTime;
+                pq.add(new Pair(pr.node,totalTime));
+            }
+        }
+       }
+       int max=-1;
+       for(int i=1;i<=n;i++){
+        if(ans[i]==Integer.MAX_VALUE) return -1;
+        if(ans[i]>max) max=ans[i];
+       }
+       return max;
+    }
+}
