c76126535d7f3e3f766aed494670df6e2d3f2224
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2026-04-29 01:14:00 +0530

    Time: 46 ms (65.30%) | Memory: 65.8 MB (90.53%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,40 +1,16 @@
 class Solution {
-    Random randomNo = new Random();
     public int findKthLargest(int[] nums, int k) {
-        //Brute-with sorting
-        // int n=nums.length;
-        // Arrays.sort(nums);
-        // return nums[n-k];
-
-        int low = Integer.MAX_VALUE, high = Integer.MIN_VALUE;
-
-        // Step 1: Find min and max in array
-        for (int n : nums) {
-            low = Math.min(low, n);
-            high = Math.max(high, n);
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<k;i++){
+            pq.add(nums[i]);
         }
-
-        // Step 2: Binary search on value range
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int count = 0;
-
-            // Count elements >= mid
-            for (int n : nums) {
-                if (n >= mid) {
-                    count++;
-                }
+        for(int i=k;i<nums.length;i++){
+            if(nums[i]>pq.peek()){
+                pq.poll();
+                pq.add(nums[i]);
             }
-
-            // Adjust search space
-            if (count >= k) {
-                low = mid + 1; 
-            } else {
-                high = mid - 1; 
-            }
+            if(nums[i]<=pq.peek()) continue;
         }
-
-        // Step 3: 'high' is the kth largest
-        return high;
+        return pq.peek();
     }
-    }
+}
