ff45fcba81b8c17a43ab02323e36eea8d882c0c7
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2026-08-30 16:33:38 +0530

    Time: 23 ms (29.97%) | Memory: 72.4 MB (11.35%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,31 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        int i = 0;
-        while (i < nums.length) {
-            int index = nums[i] - 1;
-            if (nums[i] != nums[index]) {
-                swap(nums, i, index);
-            } else {
-                i++;
-            }
-        }
-
-        List<Integer> list = new ArrayList<>();
-        // Search for missing numbers
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != j + 1) {
-                list.add(nums[j]);
-            }
+        List<Integer> list=new ArrayList<>();
+        Set<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(!set.contains(nums[i])) set.add(nums[i]);
+            else if(set.contains(nums[i])) list.add(nums[i]);
         }
         return list;
-    }
-
-    public void swap(int[] nums, int i, int index) {
-        int temp = nums[i];
-        nums[i] = nums[index];
-        nums[index] = temp;
     }
 }
